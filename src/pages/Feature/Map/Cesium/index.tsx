eff34cd6f7d75a664df4c1f5480b94e36e85a7a7
Jerry <lishuai4415@gmail.com>
2024-05-13 09:25:39 +0800

    fix: cesium destroy

--- src/pages/Feature/Map/Cesium/index.tsx
+++ src/pages/Feature/Map/Cesium/index.tsx
@@ -1,35 +1,37 @@
 import { ProCard } from '@ant-design/pro-components';
-import { useMount } from 'ahooks';
+// import { useMount } from 'ahooks';
 import {
-  Cartesian3,
-  Math as CesiumMath,
   Ion,
-  Terrain,
+  // Terrain,
   Viewer,
-  createOsmBuildingsAsync,
+  // createOsmBuildingsAsync,
 } from 'cesium';
 import 'cesium/Build/Cesium/Widgets/widgets.css';
+import { useEffect } from 'react';
 
 export default function CesiumMap() {
   Ion.defaultAccessToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZjBmZTAzMS1jMTJkLTQ5OGQtYTkyYS0xODcyMjU3MTQzMmUiLCJpZCI6MTg2MDcxLCJpYXQiOjE3MDM0MDIwMzl9.DQzBf8T_0U92DwjBcYQqdgwhFs-Cx7EacplwoL_ypjY';
 
-  useMount(async () => {
+  useEffect(() => {
     const viewer = new Viewer('cesiumContainer', {
-      terrain: Terrain.fromWorldTerrain(),
+      // terrain: Terrain.fromWorldTerrain(),
     });
 
-    viewer.camera.flyTo({
-      destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-      orientation: {
-        heading: CesiumMath.toRadians(0.0),
-        pitch: CesiumMath.toRadians(-15.0),
-      },
-    });
+    // viewer.camera.flyTo({
+    //   destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    //   orientation: {
+    //     heading: CesiumMath.toRadians(0.0),
+    //     pitch: CesiumMath.toRadians(-15.0),
+    //   },
+    // });
 
-    const buildingTileset = await createOsmBuildingsAsync();
-    viewer.scene.primitives.add(buildingTileset);
-  });
+    // const buildingTileset = await createOsmBuildingsAsync();
+    // viewer.scene.primitives.add(buildingTileset);
+    return () => {
+      viewer.destroy();
+    };
+  }, []);
 
   return (
     <ProCard>
