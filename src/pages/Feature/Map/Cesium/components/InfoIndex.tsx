973e708bf8334a88ed6bcc8d979ca2f93cf170ca
Jerry <lishuai4415@gmail.com>
2024-05-14 11:48:42 +0800

    feat: public cesium

---
+++ src/pages/Feature/Map/Cesium/components/InfoIndex.tsx
@@ -0,0 +1,54 @@
+/**
+ * InfoIndex.tsx
+ * Cesium地图 去除所有组件
+ */
+import { ProCard } from '@ant-design/pro-components';
+import { Spin } from 'antd';
+import * as Cesium from 'cesium';
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+import { useEffect, useState } from 'react';
+
+export default function InfoIndex() {
+  const [viewer, setViewer] = useState(null as any);
+
+  Cesium.Ion.defaultAccessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZjBmZTAzMS1jMTJkLTQ5OGQtYTkyYS0xODcyMjU3MTQzMmUiLCJpZCI6MTg2MDcxLCJpYXQiOjE3MDM0MDIwMzl9.DQzBf8T_0U92DwjBcYQqdgwhFs-Cx7EacplwoL_ypjY';
+
+  useEffect(() => {
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+      // 去除所有的控件
+      animation: false, // 是否显示动画控件
+      baseLayerPicker: false, // 是否显示图层选择控件
+      fullscreenButton: false, // 是否显示全屏按钮
+      geocoder: false, // 是否显示地名查找控件
+      homeButton: false, // 是否显示Home按钮
+      infoBox: false, // 是否显示信息框
+      sceneModePicker: false, // 是否显示3D/2D选择器
+      selectionIndicator: false, // 是否显示选取指示器组件
+      timeline: false, // 是否显示时间轴
+      navigationHelpButton: false, // 是否显示帮助信息按钮
+      navigationInstructionsInitiallyVisible: false, // 是否显示导航指示
+      scene3DOnly: true, // 是否只显示3D
+      shouldAnimate: true, // 是否显示动画
+      skyAtmosphere: false, // 是否显示大气层
+      skyBox: false, // 是否显示天空盒
+      vrButton: false, // 是否显示VR按钮
+    });
+
+    // 去除版权信息
+    (viewer.cesiumWidget.creditContainer as HTMLElement).style.display = 'none';
+
+    setViewer(viewer);
+
+    return () => {
+      viewer.destroy();
+    };
+  }, []);
+
+  return (
+    <ProCard>
+      {viewer === null && <Spin spinning={true} />}
+      <div id="cesiumContainer" />
+    </ProCard>
+  );
+}
--- src/pages/Feature/Map/Cesium/index.tsx
+++ src/pages/Feature/Map/Cesium/index.tsx
@@ -1,41 +1,8 @@
-import { ProCard } from '@ant-design/pro-components';
-// import { useMount } from 'ahooks';
-import {
-  Ion,
-  // Terrain,
-  Viewer,
-  // createOsmBuildingsAsync,
-} from 'cesium';
-import 'cesium/Build/Cesium/Widgets/widgets.css';
-import { useEffect } from 'react';
+/**
+ * Cesium地图 index
+ */
+import InfoIndex from './components/InfoIndex';
 
 export default function CesiumMap() {
-  Ion.defaultAccessToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZjBmZTAzMS1jMTJkLTQ5OGQtYTkyYS0xODcyMjU3MTQzMmUiLCJpZCI6MTg2MDcxLCJpYXQiOjE3MDM0MDIwMzl9.DQzBf8T_0U92DwjBcYQqdgwhFs-Cx7EacplwoL_ypjY';
-
-  useEffect(() => {
-    const viewer = new Viewer('cesiumContainer', {
-      // terrain: Terrain.fromWorldTerrain(),
-    });
-
-    // viewer.camera.flyTo({
-    //   destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-    //   orientation: {
-    //     heading: CesiumMath.toRadians(0.0),
-    //     pitch: CesiumMath.toRadians(-15.0),
-    //   },
-    // });
-
-    // const buildingTileset = await createOsmBuildingsAsync();
-    // viewer.scene.primitives.add(buildingTileset);
-    return () => {
-      viewer.destroy();
-    };
-  }, []);
-
-  return (
-    <ProCard>
-      <div id="cesiumContainer" />
-    </ProCard>
-  );
+  return <InfoIndex />;
 }
