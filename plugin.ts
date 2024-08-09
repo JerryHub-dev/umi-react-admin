ea26d0210575f3f5e564ff902c9959b4acf6ab36
Jerry <lishuai4415@gmail.com>
2024-08-09 18:18:01 +0800

    feat: add clarity

--- plugin.ts
+++ plugin.ts
@@ -5,7 +5,15 @@ export default (api: IApi) => {
   api.modifyHTML(($) => {
     $('head').append([
       // 缩小的 turf 文件，并将其包含在脚本标记中
-      '<script src="/public/js/turf.min.js" charset="utf-8"></script>',
+      `<script src="/public/js/turf.min.js" charset="utf-8"></script>`,
+      // 添加 clarity 标签
+      `<script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "nkjea8mkrt");
+      </script>`,
     ]);
     return $;
   });
