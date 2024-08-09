b3dcd9ff44715f8e2d1403dad5edc6d6026ae22d
kingling-abb <153783999+kingling-abb@users.noreply.github.com>
2024-08-09 12:03:40 +0000

    Deploying to gh-pages from @ JerryHub-dev/umi-react-admin@d4965c5104c940a3c25cc1102d2b484266c52bde 🚀

---
+++ js/clarity.js
@@ -0,0 +1,12 @@
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  const t1 = l.createElement(r);
+  t1.async = 1;
+  t1.src = 'https://www.clarity.ms/tag/' + i;
+  const y1 = l.getElementsByTagName(r)[0];
+  y1.parentNode.insertBefore(t1, y1);
+})(window, document, 'clarity', 'script', 'nkjea8mkrt');
