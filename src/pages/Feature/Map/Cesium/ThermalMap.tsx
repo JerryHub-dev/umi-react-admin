138ae4e4bfea805f144e87c5bc48b85a9a6bf570
Jerry <lishuai4415@gmail.com>
2024-09-28 20:15:53 +0800

    feat: thermal map icon add

--- src/pages/Feature/Map/Cesium/ThermalMap.tsx
+++ src/pages/Feature/Map/Cesium/ThermalMap.tsx
@@ -210,6 +210,30 @@ const ThermalMap = () => {
     let longitude = 106.65;
     let latitude = 29.69;
 
+    viewer.entities.add({
+      position: Cesium.Cartesian3.fromDegrees(longitude, latitude),
+      // id: 'thermal',
+      billboard: {
+        image: require('@/assets/Detection.png'),
+        scale: 0.3,
+      },
+      // label: {
+      //   text: '热力图', // 文本内容
+      //   font: '14px sans-serif', // 字体大小
+      //   backgroundColor: Cesium.Color.fromCssColorString('#0094ff'), // 背景颜色
+      //   showBackground: true, // 是否显示背景
+      //   style: Cesium.LabelStyle.FILL_AND_OUTLINE, // 样式
+      //   fillColor: Cesium.Color.WHITE, // 填充颜色
+      //   outlineColor: Cesium.Color.BLACK, // 边框颜色
+      //   outlineWidth: 2, // 边框宽度
+      //   horizontalOrigin: Cesium.HorizontalOrigin.CENTER, // 水平对齐方式
+      //   verticalOrigin: Cesium.VerticalOrigin.BOTTOM, // 垂直对齐方式
+      //   pixelOffset: new Cesium.Cartesian2(0, 55), // 偏移量
+      //   pixelOffsetScaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.5, 8.0e6, 0.5), // 偏移量随距离变化
+      // },
+    });
+
+    // 开始处理 base64 图片数据
     let base64Image = 'data:image/png;base64,' + obj.diagramPngStream; // base64 图片数据
 
     // 将 Base64 数据转换为 Blob 对象
