8d3b1e0859f4a2f7412c1adb200ef2664ef8009e
Jerry <lishuai4415@gmail.com>
2024-12-19 21:00:58 +0800

    fix: signature image/png error

--- src/pages/Feature/Signature/index.tsx
+++ src/pages/Feature/Signature/index.tsx
@@ -19,7 +19,7 @@ export default function Signature() {
       let ratio = Math.max(window.devicePixelRatio || 1, 1);
       canvas.width = canvas.offsetWidth * ratio;
       canvas.height = canvas.offsetHeight * ratio;
-      canvas.getContext('2d').scale(ratio, ratio);
+      canvas.getContext('2d')?.scale(ratio, ratio);
     }
 
     window.onresize = resizeCanvas;
@@ -35,33 +35,57 @@ export default function Signature() {
         return alert('请先提供签名.');
       }
 
-      let data = signaturePad.toDataURL('image/png');
-      console.log(data);
-      window.open(data);
-    });
+      // let data = signaturePad.toDataURL('image/png');
+      // console.log(data);
+      // window.open(data);
+      // 辅助函数
+      function dataURLToBlob(dataURL: any) {
+        const parts = dataURL.split(';base64,');
+        const contentType = parts[0].split(':')[1];
+        const raw = window.atob(parts[1]);
+        const rawLength = raw.length;
+        const uInt8Array = new Uint8Array(rawLength);
 
-    let saveJpegButton = document.getElementById('save-jpeg') as HTMLButtonElement;
-    saveJpegButton.addEventListener('click', function () {
-      if (signaturePad.isEmpty()) {
-        return alert('请先提供签名.');
+        for (let i = 0; i < rawLength; ++i) {
+          uInt8Array[i] = raw.charCodeAt(i);
+        }
+
+        return new Blob([uInt8Array], { type: contentType });
       }
-
-      let data = signaturePad.toDataURL('image/jpeg');
-      console.log(data);
-      window.open(data);
+      const dataURL = signaturePad.toDataURL('image/png');
+      const blob = dataURLToBlob(dataURL);
+      const url = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.download = 'signature.png';
+      link.href = url;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
     });
 
-    let saveSvgButton = document.getElementById('save-svg') as HTMLButtonElement;
-    saveSvgButton.addEventListener('click', function () {
-      if (signaturePad.isEmpty()) {
-        return alert('请先提供签名.');
-      }
+    // let saveJpegButton = document.getElementById('save-jpeg') as HTMLButtonElement;
+    // saveJpegButton.addEventListener('click', function () {
+    //   if (signaturePad.isEmpty()) {
+    //     return alert('请先提供签名.');
+    //   }
 
-      let data = signaturePad.toDataURL('image/svg+xml');
-      console.log(data);
-      console.log(atob(data.split(',')[1]));
-      window.open(data);
-    });
+    //   let data = signaturePad.toDataURL('image/jpeg');
+    //   console.log(data);
+    //   window.open(data);
+    // });
+
+    // let saveSvgButton = document.getElementById('save-svg') as HTMLButtonElement;
+    // saveSvgButton.addEventListener('click', function () {
+    //   if (signaturePad.isEmpty()) {
+    //     return alert('请先提供签名.');
+    //   }
+
+    //   let data = signaturePad.toDataURL('image/svg+xml');
+    //   console.log(data);
+    //   console.log(atob(data.split(',')[1]));
+    //   window.open(data);
+    // });
 
     let clear = document.getElementById('clear') as HTMLButtonElement;
     clear.addEventListener('click', function () {
@@ -101,8 +125,8 @@ export default function Signature() {
 
         <div className="flex justify-between max-w-3xl mt-8 min-w-96 mb-28">
           <Button id="save-png">保存为 PNG</Button>
-          <Button id="save-jpeg">保存为 JPEG</Button>
-          <Button id="save-svg">保存为 SVG</Button>
+          {/* <Button id="save-jpeg">保存为 JPEG</Button>
+          <Button id="save-svg">保存为 SVG</Button> */}
           <Button id="draw">画</Button>
           <Button id="erase">擦除</Button>
           <Button id="undo">撤销</Button>
